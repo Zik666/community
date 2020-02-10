6a40918cab3e65d2344aa315987f80b84a9efd84
Zik666 <1770703320@qq.com>
2020-02-11 02:34:50 +0800

    add check cookies

--- src/main/java/com/zik666/community/controller/IndexController.java
+++ src/main/java/com/zik666/community/controller/IndexController.java
@@ -1,7 +1,13 @@
 package com.zik666.community.controller;
 
+import com.zik666.community.mapper.UserMapper;
+import com.zik666.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Zik666
@@ -9,8 +15,21 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName())) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
